da4bb71b8c19f2fdb52d63fb92d35f688062db36
FernasG <fernandogoul7@gmail.com>
2021-12-28 11:48:40 -0300

    feat: add docker compose to script

--- ubuntu.sh
+++ ubuntu.sh
@@ -20,17 +20,17 @@ echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sou
 sudo apt -qq install spotify-client -y
 
 printf "\nDocker...\n"
-sudo apt -qq install \
-    ca-certificates \
-    gnupg \
-    lsb-release -y
+sudo apt -qq install ca-certificates gnupg lsb-release -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 sudo apt -qq update
-sudo apt -qq install docker-ce docker-ce-cli containerd.io
-
+sudo apt -qq install docker-ce docker-ce-cli containerd.io -y
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+printf "\nDocker Compose...\n"
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 printf "\nStyles...\n"
 sudo apt -qq install gnome-tweaks git gnome-shell-extensions -y
