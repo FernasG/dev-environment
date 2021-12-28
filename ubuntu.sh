b714ab2504880966b28d32236aa856c81f15d0c8
FernasG <fernandogoul7@gmail.com>
2021-12-28 13:11:22 -0300

    feat: add beekeper studio to script

--- ubuntu.sh
+++ ubuntu.sh
@@ -5,25 +5,29 @@ sudo apt -qq update
 sudo apt -qq upgrade -y
 
 printf "\nVisual Studio Code...\n"
-wget -O $HOME/Downloads/vscode.deb 'https://update.code.visualstudio.com/latest/linux-deb-x64/stable'
+wget --quiet -O $HOME/Downloads/vscode.deb 'https://update.code.visualstudio.com/latest/linux-deb-x64/stable'
 sudo dpkg -i $HOME/Downloads/vscode.deb
 
 printf "\nNVM - NodeJS...\n"
+sudo apt -qq install curl -y
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 printf "\nSpotify...\n"
-sudo apt -qq install curl -y
 curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt -qq update && sudo apt -qq install spotify-client -y
 
+printf "\nBeekeper...\n"
+wget --quiet -O - https://deb.beekeeperstudio.io/beekeeper.key | sudo apt-key add -
+echo "deb https://deb.beekeeperstudio.io stable main" | sudo tee /etc/apt/sources.list.d/beekeeper-studio-app.list
+sudo apt -qq update && sudo apt -qq install beekeeper-studio -y
+
 printf "\nDocker...\n"
 sudo apt -qq install ca-certificates gnupg lsb-release -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-sudo apt -qq update
-sudo apt -qq install docker-ce docker-ce-cli containerd.io -y
+sudo apt -qq update && sudo apt -qq install docker-ce docker-ce-cli containerd.io -y
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
