3fd1591a14f9bbb7e78f9441735e5b7f67a80ab7
FernasG <fernandogoul7@gmail.com>
2021-12-28 11:26:50 -0300

    feat: add docker

--- ubuntu.sh
+++ ubuntu.sh
@@ -8,7 +8,7 @@ printf "\nVisual Studio Code...\n"
 wget -O $HOME/Downloads/vscode.deb 'https://update.code.visualstudio.com/latest/linux-deb-x64/stable'
 sudo dpkg -i $HOME/Downloads/vscode.deb
 
-printf "\nNVM - NodeJS..."
+printf "\nNVM - NodeJS...\n"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
@@ -18,6 +18,20 @@ sudo apt -qq install curl -y
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt -qq install spotify-client -y
+
+printf "\nDocker...\n"
+sudo apt -qq install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo apt -qq update
+sudo apt -qq install docker-ce docker-ce-cli containerd.io
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 printf "\nStyles...\n"
 sudo apt -qq install gnome-tweaks git gnome-shell-extensions -y
